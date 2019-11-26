44ed6d292a62dce07b44076d6a1861fa8d7e3f34
Snehansh Akhaury <snehansh@protonmail.com>
2019-11-25 19:38:33 -0600

    Add project files.

---
+++ TestProject/Window1Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using WPFUITesting;
+
+namespace TestProject
+{
+    [Apartment(ApartmentState.STA)]
+    [TestFixture]
+    public class Window1Tests
+    {
+        private ButtonAutomationPeer buttonPeer;
+        private TextBoxAutomationPeer textBoxPeer;
+        private MainWindow window;
+        private WindowAutomationPeer windowPeer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            window = new MainWindow();
+            window.Show();
+            windowPeer = new WindowAutomationPeer(window);
+            List<AutomationPeer> children = windowPeer.GetChildren();
+            buttonPeer = (ButtonAutomationPeer)children[0];
+            textBoxPeer = (TextBoxAutomationPeer)children[1];
+        }
+
+        [Test]
+        public void ClickingIncrementIncrementsTheNumberInTheTextBox()
+        {
+            Assert.AreEqual("0", ((IValueProvider)textBoxPeer).Value);
+            Button button = (Button)buttonPeer.Owner;
+            RoutedEventArgs args = new RoutedEventArgs(Button.ClickEvent, button);
+            button.RaiseEvent(args);
+            Assert.AreEqual("1", ((IValueProvider)textBoxPeer).Value);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            window.Close();
+        }
+    }
+}
---
+++ WPFUITesting/NumberHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUITesting
+{
+    public class NumberHolder : INotifyPropertyChanged
+    {
+        private int number = 0;
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                FirePropertyChanged("Number");
+            }
+        }
+
+        private void FirePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
